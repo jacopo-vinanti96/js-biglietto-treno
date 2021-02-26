b2951ac9dcbd3bb9f924b7270133780a468647fc
Jacopo Vinanti <jacopovinanti@gmail.com>
2021-02-26 17:00:48 +0100

    Aggiunte
    - Controllo che i valori siano numerici
    - Aggiunti commenti js

--- js/script.js
+++ js/script.js
@@ -1,11 +1,26 @@
-// Dichiarazione variabili
-var km = parseInt( prompt("Inserisci la distanza (km) del percorso") ),
-age = parseInt( prompt("Inserisci l' età del passeggero") ),
-prezzo = ( km * 0.21 ),
-scontoU18 = (  km * 0.21 * 0.20 ),
-scontoO65 = (  km * 0.21 * 0.40);
 
+//Dichiarazione e controllo variabile km inserita dall' utente
+var km = prompt("Inserisci la distanza (km) del percorso");
 
+while ( isNaN(km) ) {
+  alert("La distanza inserita non è un valore numerico");
+  var km = prompt("Inserisci la distanza (km) del percorso");
+}
+
+//Dichiarazione e controllo variabile age inserita dall' utente
+var age = prompt("Inserisci l' età del passeggero");
+
+while ( isNaN(age) ) {
+  alert("L' età inserita non è un valore numerico");
+  var age = prompt("Inserisci l' età del passeggero");
+}
+
+//Calcolo valori di sconto e prezzo
+var prezzo = ( km * 0.21 ),
+    scontoU18 = (  km * 0.21 * 0.20 ),
+    scontoO65 = (  km * 0.21 * 0.40);
+
+//Calcolo prezzo scontato
 if ( age < 18 ) {
   prezzo = prezzo - scontoU18;
   alert("il passeggero ha diritto a uno sconto del 20%");
@@ -14,4 +29,5 @@ if ( age < 18 ) {
   alert("il passeggero ha diritto a uno sconto del 40%");
 }
 
-alert( prezzo.toFixed(2) + "€" );
+//Risultato a schermo
+alert( "Il prezzo del biglietto è: " + prezzo.toFixed(2) + "€" );
