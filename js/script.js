e48fe43898783a8f2d9fd3b4e59baf2fa84769b5
Jacopo Vinanti <jacopovinanti@gmail.com>
2021-02-26 15:15:25 +0100

    Fix
    - NaN con calcoli prezzo

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-var km = prompt("Inserisci la distanza (km) del percorso"), age = prompt("Inserisci l' età del passeggero"), prezzo = ( km * 0.21 ) + "€", scontoU18 = (  km * 0.21 * 0.20), scontoO65 = (  km * 0.21 * 0.40);
+var km = prompt("Inserisci la distanza (km) del percorso"), age = prompt("Inserisci l' età del passeggero"), prezzo = ( km * 0.21 ), scontoU18 = (  km * 0.21 * 0.20), scontoO65 = (  km * 0.21 * 0.40);
 
 if ( age < 18 ) {
   prezzo = prezzo - scontoU18;
@@ -8,4 +8,4 @@ if ( age >= 65  ) {
   prezzo = prezzo - scontoO65;
 }
 
-alert(prezzo)
+alert(prezzo + "€")
