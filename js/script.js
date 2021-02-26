212c09eab984d01f6e604d28df3bfd69f7ae74c0
Jacopo Vinanti <jacopovinanti@gmail.com>
2021-02-26 16:32:38 +0100

    Fix, pulizia

--- js/script.js
+++ js/script.js
@@ -1,11 +1,17 @@
-var km = prompt("Inserisci la distanza (km) del percorso"), age = prompt("Inserisci l' età del passeggero"), prezzo = ( km * 0.21 ), scontoU18 = (  km * 0.21 * 0.20), scontoO65 = (  km * 0.21 * 0.40);
+// Dichiarazione variabili
+var km = parseInt( prompt("Inserisci la distanza (km) del percorso") ),
+age = parseInt( prompt("Inserisci l' età del passeggero") ),
+prezzo = ( km * 0.21 ),
+scontoU18 = (  km * 0.21 * 0.20 ),
+scontoO65 = (  km * 0.21 * 0.40);
+
 
 if ( age < 18 ) {
   prezzo = prezzo - scontoU18;
-}
-
-if ( age >= 65  ) {
+  alert("il passeggero ha diritto a uno sconto del 20%");
+} else if ( age >= 65  ) {
   prezzo = prezzo - scontoO65;
+  alert("il passeggero ha diritto a uno sconto del 40%");
 }
 
-alert(prezzo + "€");
+alert( prezzo.toFixed(2) + "€" );
