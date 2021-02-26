ca1b84d7c960bbf635396a207388a2daa725cc9c
Jacopo Vinanti <jacopovinanti@gmail.com>
2021-02-26 15:12:18 +0100

    Esercizio funzionante

---
+++ js/script.js
@@ -0,0 +1,11 @@
+var km = prompt("Inserisci la distanza (km) del percorso"), age = prompt("Inserisci l' età del passeggero"), prezzo = ( km * 0.21 ) + "€", scontoU18 = (  km * 0.21 * 0.20), scontoO65 = (  km * 0.21 * 0.40);
+
+if ( age < 18 ) {
+  prezzo = prezzo - scontoU18;
+}
+
+if ( age >= 65  ) {
+  prezzo = prezzo - scontoO65;
+}
+
+alert(prezzo)
